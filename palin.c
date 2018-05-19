da21848f6db4ed74fe80c8e184fbaf9148f6c747
SindhuR0910 <35699139+SindhuR0910@users.noreply.github.com>
2018-05-19 00:50:40 -0700

    Add files via upload

---
+++ palin.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+
+{
+    char a[50],b[50];
+    printf("\n enter the string");
+    gets(a);
+    strcpy(b,a);
+    strrev(b);
+    if(strcmp(a,b)==0)
+    printf("palindrome");
+    else
+    printf("not palindrome");
+    return 0;
+}
+
